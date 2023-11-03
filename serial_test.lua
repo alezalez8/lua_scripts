8a5835789c4a9568c1ee0514108ad1015b78f5c5
alezalez8 <5446943@ukr.net>
2023-11-04 00:53:30 +0200

    4_november

--- serial_test.lua
+++ serial_test.lua
@@ -3,6 +3,7 @@ port:begin(9200)
 port:set_flow_control(0)
 
 local step = 0
+local invStep = 100
 local RUN_INTERVAL_MS = 10 -- 10ms == 100 Hz
 
 
@@ -11,13 +12,17 @@ function spit()
   --if port:available() > 0 then
   --read = port:read()
 
-  gcs:send_named_float("latit", 1234567.89)
+  gcs:send_named_float("latit", (invStep - step + 100))
   gcs:send_named_float("longtid", 987654321)
   gcs:send_named_float("counter", step)
-  gcs:send_named_float("MY_DATA", 123456.789)
+  gcs:send_named_float("MY_DATA", invStep)
   step = step + 1
+  invStep = invStep - 1
   if step > 100 then
     step = 0
+  end
+  if invStep == 0 then
+    invStep = 100
   end
 
   return spit, RUN_INTERVAL_MS
