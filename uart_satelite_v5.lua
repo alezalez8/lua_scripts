987a0709a5001aa3e0308cab46c8d6057e4b7b96
alezalez8 <5446943@ukr.net>
2023-11-13 22:32:53 +0200

    13_november

--- uart_satelite_v5.lua
+++ uart_satelite_v5.lua
@@ -1,26 +1,54 @@
 -- Создание таблицы с данными (в данном случае, массив чисел)
-local data = { 1, 2, 3, 4, 5, 6, ... } -- Замените ... на остальные значения
+local data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } -- Замените ... на остальные значения
 
--- Создание Mavlink-пакета
-local mavlink_msg = {
-   target_system = 1,     -- Замените на ваш ID системы
-   target_component = 1,  -- Замените на ваш ID компонента
-   seq = 0,               -- Последовательный номер пакета
-   sysid = 1,             -- ID системы отправителя
-   compid = 1,            -- ID компонента отправителя
-   length = 8 * #data,    -- Длина данных в байтах
+local temp = 0
+local myCustomMessageID = 200
 
-   -- Тип сообщения и подтип (пользовательский кастомный тип)
-   msgid = 150,  -- MAVLINK_MSG_ID_USER (замените на нужный вам ID)
 
-   -- Payload64
-   payload64 = {}
-}
+function convertData(data)
+   -- Создание Mavlink-пакета
+   local mavlink_msg = {
+      target_system = 1,    -- Замените на ваш ID системы
+      target_component = 1, -- Замените на ваш ID компонента
+      seq = 0,              -- Последовательный номер пакета
+      sysid = 1,            -- ID системы отправителя
+      compid = 1,           -- ID компонента отправителя
+      length = 8 * #data,   -- Длина данных в байтах
 
--- Заполнение payload64 данными
-for i = 1, #data do
-   mavlink_msg.payload64[i] = data[i]
+      -- Тип сообщения и подтип (пользовательский кастомный тип)
+      msgid = 150, -- MAVLINK_MSG_ID_USER (замените на нужный вам ID)
+
+      -- Payload64
+      payload64 = {}
+   }
+
+   -- Заполнение payload64 данными
+   for i = 1, #data do
+      mavlink_msg.payload64[i] = data[i]
+   end
+
+   -- Преобразование Mavlink-пакета в строку
+   local mavlink_str = string.pack("<BBBBIIB", mavlink_msg.sysid, mavlink_msg.compid, mavlink_msg.seq, mavlink_msg.msgid,
+      mavlink_msg.target_system, mavlink_msg.target_component, mavlink_msg.length)
+   --формат строки (например, "<BBBBIIB") зависит от структуры вашего Mavlink-пакета
+
+
+   -- Добавление payload64 к строке
+   for _, value in ipairs(mavlink_msg.payload64) do
+      mavlink_str = mavlink_str .. string.pack("<I", value)
+   end
+   return mavlink_str
 end
 
--- Отправка Mavlink-пакета
-mavlink.send_chan(0, mavlink_chan, mavlink.MAVLINK_MSG_ID_USER, mavlink_msg)
+function update()
+   local myData = convertData(data)
+   mavlink:send_chan(0, myCustomMessageID, myData)
+   gcs:send_text(0, "All sent  " .. temp)
+   temp = temp + 1
+   if temp >= 20 then
+      temp = 0
+   end
+   return update, 200
+end
+
+return update, 1000
