7644410d4444437f229a19445309b359b105e880
alezalez8 <5446943@ukr.net>
2023-12-04 20:53:30 +0200

    4_december

--- logger_pro_mini_with_handler/logger_pro_mini_with_handler.ino
+++ logger_pro_mini_with_handler/logger_pro_mini_with_handler.ino
@@ -3,12 +3,12 @@
 
 /*
  * -------------------------------- Pin level ----------------------
- * Level 1                              D7                           D21
- * Level 2                              D8                           D22
- * Level 3                              D9                           D23
- * Level 4                              D10                          D24
- * Level 5                              D11                          D25
- * Level 6                              D12                          D26
+ * Level 1                              D7                          
+ * Level 2                              D8                          
+ * Level 3                              D9                           
+ * Level 4                              D10                          
+ * Level 5                              D11                          
+ * Level 6                              D12                         
  *
  * -------------------------------- Pin control input ---------------
  * Mode switch input                                                 D2
@@ -21,8 +21,10 @@
  * ON/OFF detector                                                   D10
 
 */
-uint32_t timerLevel = 0; // timer for LEVEL
-#define T_PERIOD_LEVEL 5 // time of invoke data from sensor of level, mS.
+uint32_t timerLevel = 0;  // timer for LEVEL
+uint32_t timerLevelTest = 0;
+#define T_PERIOD_LEVEL 10  // time of invoke data from sensor of level, mS.
+#define T_PERIOD_LEVEL_TEST 200
 
 uint32_t timeInvokeSensitive = 0;
 uint32_t timeInvokeSensPeriod = 15;
@@ -31,7 +33,7 @@ uint32_t timeInvokeMode = 0;
 uint32_t timeInvokeModePeriod = 50;
 int currentLevel = 1;
 
-const int arraySize = 10;
+const int arraySize = 20;
 int dataArray[arraySize];
 int dataIndex = 0;
 
@@ -51,16 +53,15 @@ const int sensitiveOutput = 6;
 const int modeDetectorOutput = 5;
 
 // ------------- sygnal from FC ------------------------
-const int modeDetectorInput = 2;      // mode of detector
-const int sensitiveDetectorInput = 3; // sensitive of detector
+const int modeDetectorInput = 2;       // mode of detector
+const int sensitiveDetectorInput = 3;  // sensitive of detector
 const int resetDetectorInput = 4;
 
 // ----------- for interrupt -----------------------
-volatile unsigned long pulseWidth1 = 0; // time of PPM of S11 on FC - MODE
-volatile unsigned long pulseWidth2 = 0; // time of PPM of S12 on FC - SENSITIVE
+volatile unsigned long pulseWidth1 = 0;  // time of PPM of S11 on FC - MODE
+volatile unsigned long pulseWidth2 = 0;  // time of PPM of S12 on FC - SENSITIVE
 
-void setup()
-{
+void setup() {
 
   pinMode(resetOutput, OUTPUT);
   pinMode(resetDetectorInput, INPUT_PULLUP);
@@ -76,66 +77,59 @@ void setup()
 
   attachInterrupt(digitalPinToInterrupt(modeDetectorInput), handleInterrupt1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(sensitiveDetectorInput), handleInterrupt2, CHANGE);
-
-  for (int i = 0; i < 10; i++)
-  {
-    dataArray[i] = 1;
-  }
+ 
+  arrayInit();
 
   randomSeed(analogRead(0));
-  Serial.begin(19200);
+  Serial.begin(57600);
 }
 
-void loop()
-{
+void loop() {
 
-  Serial.println("Helloy");
-
-  if (millis() - timeInvokeSensitive >= timeInvokeSensPeriod)
-  {
+  if (millis() - timeInvokeSensitive >= timeInvokeSensPeriod) {
     timeInvokeSensitive = millis();
     adjustSensitive(pulseWidth2);
   }
 
-  if (millis() - timeInvokeMode >= timeInvokeModePeriod)
-  {
+  if (millis() - timeInvokeMode >= timeInvokeModePeriod) {
     timeInvokeMode = millis();
     modeOfDetector(pulseWidth1);
   }
 
-  if (millis() - timerLevel >= T_PERIOD_LEVEL)
-  {
-    timerLevel = millis();
-
-    // read data every 5 ms
-    getDataLevelDumpTwo();
-
-    // check if UART is ready to trancieve my data
-    if (Serial.available() > 0)
-    {
-      char readyToPass = Serial.read();
-      if (readyToPass == '5')
-      {
-        clearBuffer();
-       ledOn = !ledOn;
-       blinkLed(ledOn);
-        sendArray();
-      }
+  // -------------------------- ask FC about reading this data ------------
+  if (Serial.available() > 0) {
+    char readyToPass = Serial.read();
+    if (readyToPass == '5') {
+      clearBuffer();
+      ledOn = !ledOn;
+      blinkLed(ledOn);
+      sendArray();
     }
+  }
+
+  // if (millis() - timerLevelTest >= T_PERIOD_LEVEL_TEST) {
+  //   timerLevelTest = millis();
+  //   clearBuffer();
+  //   ledOn = !ledOn;
+  //   blinkLed(ledOn);
+  //   sendArray();
+  // }
+
+
+  // -------------------- read data every 10 ms ----------------------------
+  if (millis() - timerLevel >= T_PERIOD_LEVEL) {
+    timerLevel = millis();
+    getDataLevelNew();
   }
 }
 
 //======================подпрограммы =================================
 
-static void getDataLevel()
-{
+static void getDataLevel() {
 
-  for (int i = 12; i >= 7; i--)
-  {
-    if (digitalRead(i) == HIGH)
-    {
-      for (int j = 0; j < arraySize - 1; j++)
-      {
+  for (int i = 12; i >= 7; i--) {
+    if (digitalRead(i) == HIGH) {
+      for (int j = 0; j < arraySize - 1; j++) {
         dataArray[j] = dataArray[j + 1];
       }
       dataArray[arraySize - 1] = i - 6;
@@ -144,16 +138,33 @@ static void getDataLevel()
   }
 }
 
-static void getDataLevelDumpTwo()
-{
+static void getDataLevelNew() {
+  int currentLevel = 1;
+  for (int i = 12; i >= 7; i--) {
+    if (digitalRead(i) == HIGH) {
+      currentLevel = i - 6;
+      break;
+    }
+  }
+
+  for (int i = 0; i < arraySize - 1; i++) {
+    dataArray[i] = dataArray[i + 1];
+  }
+  dataArray[arraySize - 1] = currentLevel;
+}
+
+static void arrayInit() {
+  for (int i = 0; i < arraySize; i++) {
+    dataArray[i] = 1;
+  }
+}
+
+static void getDataLevelDumpTwo() {
   int randomLevel = random(1, 7);
 
-  for (int i = 12; i >= 7; i--)
-  {
-    if (randomLevel == i - 6)
-    {
-      for (int j = 0; j < arraySize - 1; j++)
-      {
+  for (int i = 12; i >= 7; i--) {
+    if (randomLevel == i - 6) {
+      for (int j = 0; j < arraySize - 1; j++) {
         dataArray[j] = dataArray[j + 1];
       }
       dataArray[arraySize - 1] = i - 6;
@@ -163,24 +174,19 @@ static void getDataLevelDumpTwo()
 }
 
 //---------------------------------- new dump 3 ---------------------------------
-static void getDataLevelDumpThree(int countOfPacket)
-{
+static void getDataLevelDumpThree(int countOfPacket) {
   int randomLevel = random(1, 7);
   int randomPosition = random(0, arraySize);
-  for(int i = 0; i < arraySize; i++){
+  for (int i = 0; i < arraySize; i++) {
     dataArray[i] = 1;
   }
-  if(countOfPacket == 3) {
-
+  if (countOfPacket == 3) {
   }
 
 
-  for (int i = 12; i >= 7; i--)
-  {
-    if (randomLevel == i - 6)
-    {
-      for (int j = 0; j < arraySize - 1; j++)
-      {
+  for (int i = 12; i >= 7; i--) {
+    if (randomLevel == i - 6) {
+      for (int j = 0; j < arraySize - 1; j++) {
         dataArray[j] = dataArray[j + 1];
       }
       dataArray[arraySize - 1] = i - 6;
@@ -191,95 +197,81 @@ static void getDataLevelDumpThree(int countOfPacket)
 
 //---------------------------------- end new dump 3 -----------------------------
 
-void clearBuffer()
-{
-  while (Serial.available())
-  {
+void clearBuffer() {
+  while (Serial.available()) {
     char cl = Serial.read();
   }
 }
 
 //-----------------------------------------------------------
-void sendArray()
-{
+void sendArray() {
+  // Serial.println();
+
   for (int i = 0; i < arraySize; i++)
+
   {
     Serial.print(dataArray[i]);
+    // Serial.print("  i=");
+    // Serial.print(i);
+    // Serial.print(";  ");
   }
   //Serial.println();
-  memset(dataArray, 1, sizeof(dataArray));
+  // memset(dataArray, 1, sizeof(dataArray));
+  memset(dataArray, 1, arraySize);
+  //Serial.print();
 }
 
 // -------- new handlers -------------------------------------
-void ifReset()
-{
+void ifReset() {
   boolean isRes = digitalRead(resetDetectorInput);
   digitalWrite(resetOutput, isRes);
 }
 // -------- mode of minedetector -------------------------------
-void handleInterrupt1()
-{
+void handleInterrupt1() {
 
   static unsigned long startTime1 = 0;
   unsigned long currentTime1 = micros();
 
-  if (digitalRead(modeDetectorInput) == HIGH)
-  {
+  if (digitalRead(modeDetectorInput) == HIGH) {
     startTime1 = currentTime1;
-  }
-  else
-  {
+  } else {
     pulseWidth1 = currentTime1 - startTime1;
   }
 }
-void handleInterrupt2()
-{
+void handleInterrupt2() {
 
   static unsigned long startTime2 = 0;
   unsigned long currentTime2 = micros();
 
-  if (digitalRead(sensitiveDetectorInput) == HIGH)
-  {
+  if (digitalRead(sensitiveDetectorInput) == HIGH) {
     startTime2 = currentTime2;
-  }
-  else
-  {
+  } else {
     pulseWidth2 = currentTime2 - startTime2;
   }
 }
-void printPWM(unsigned long timePWM)
-{
+void printPWM(unsigned long timePWM) {
   Serial.println();
   String message = " Time of PWM =  ";
   Serial.print(message);
   Serial.println(timePWM);
   Serial.println();
 }
-void modeOfDetector(unsigned long timePWM)
-{
+void modeOfDetector(unsigned long timePWM) {
 
-  if (timePWM >= 950 && timePWM <= 1350)
-  {
+  if (timePWM >= 950 && timePWM <= 1350) {
     analogWrite(modeDetectorOutput, 0);
-  }
-  else if (timePWM >= 1400 && timePWM <= 1700)
-  {
+  } else if (timePWM >= 1400 && timePWM <= 1700) {
     analogWrite(modeDetectorOutput, 78);
-  }
-  else if (timePWM > 1750)
-  {
+  } else if (timePWM > 1750) {
     analogWrite(modeDetectorOutput, 255);
   }
 }
-void adjustSensitive(unsigned long timePWM)
-{
+void adjustSensitive(unsigned long timePWM) {
 
-  if (timePWM < 1000)
-  {
+  if (timePWM < 1000) {
     timePWM = 1000;
   }
-  if (timePWM > 1950)
-  {
+  if (timePWM > 1950) {
     timePWM = 1950;
   }
 
@@ -287,6 +279,6 @@ void adjustSensitive(unsigned long timePWM)
   analogWrite(sensitiveOutput, valueOfSensitive);
 }
 
-void blinkLed(bool onoff){
+void blinkLed(bool onoff) {
   digitalWrite(resetOutput, onoff);
 }
